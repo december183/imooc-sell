5cf4c7bfe8862713964ad8492d364ba012eab12b
december183 <guanyue584@163.com>
2017-04-11 17:33:30 +0800

    导入基础代码

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="goods">我是goods</div>
+</template>
+<script>
+	export default{
+		name: 'goods'
+	};
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+	
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>我是seller</div>
+</template>
+<script>
+	export default{};
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+	
+</style>
