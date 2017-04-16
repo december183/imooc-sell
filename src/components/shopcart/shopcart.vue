cb957bffcb778ec63f85ddf58802497e86adcec0
december183 <guanyue584@163.com>
2017-04-16 22:04:50 +0800

    shopcart.vue购物车组件

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,91 @@
+<template>
+	<div class="shopcart">
+		<div class="content-wrapper">
+			<div class="content-left">
+				<div class="logo-wrapper">
+					<div class="logo">
+						<i class="icon-shopping_cart"></i>
+					</div>
+				</div>
+				<div class="price">￥0</div>
+				<div class="delivery">另需配送费￥{{deliveryPrice}}元</div>
+			</div>
+			<div class="content-right">
+				
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default{
+		name: 'shopcart',
+		props: {
+			deliveryPrice: {
+				type: Number,
+				default: 0
+			},
+			minPrice: {
+				type: Number,
+				default: 0
+			}
+		}
+	};
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+	.shopcart
+		position: fixed
+		left: 0
+		bottom: 0
+		z-index: 50
+		width: 100%
+		height: 48px
+		.content-wrapper
+			display: flex
+			background: #141d27
+			font-size: 0
+			.content-left
+				flex: 1
+				.logo-wrapper
+					display: inline-block
+					position: relative
+					top: -10px
+					margin: 0 12px
+					padding: 6px
+					width: 56px
+					height: 56px
+					box-sizing: border-box
+					vertical-align: top
+					border-radius: 50%
+					background: #141d27
+					.logo
+						width: 100%
+						height: 100%
+						border-radius: 50%
+						text-align: center
+						background: #2b343c
+						.icon-shopping_cart
+							line-height: 44px
+							font-size: 24px
+							color: #80858a
+				.price
+					display: inline-block
+					vertical-align: top
+					margin-top: 12px
+					line-height: 24px
+					padding-right: 12px
+					box-sizing: border-box
+					border-right: 1px solid rgba(255, 255, 255, 0.1)
+					font-size: 16px
+					font-weight: 700
+					color: rgba(255, 255, 255, 0.4)
+				.delivery
+					display: inline-block
+					vertical-align: top
+					line-height: 24px
+					margin: 12px 0 0 12px
+					color: rgba(255, 255, 255, 0.4)
+					font-size: 10px
+			.content-right
+				flex: 0 0 105px
+				width: 105px
+</style>
