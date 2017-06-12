d6cb2ae25426ed9b5e0ada739f3604701625f56e
december183 <guanyue584@163.com>
2017-06-13 05:46:17 +0800

    完善商品详情模块

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -33,6 +33,19 @@
         <div class="rating">
           <h1 class="title">商品评价</h1>
           <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+          <div class="rating-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+              <li v-show="needShow(rating.rateType, rating.text)" class="rating-item border-1px" v-for="rating in food.ratings">
+                <div class="user">
+                  <span class="name">{{rating.username}}</span>
+                  <img class="avatar" width="12" height="12" :src="rating.avatar" />
+                </div>
+                <div class="time">{{rating.rateTime | formatDate}}</div>
+                <p class="text"><span :class="{'icon-thumb_up': rating.rateType === 0, 'icon-thumb_down': rating.rateType === 1}"></span>{{rating.text}}</p>
+              </li>
+            </ul>
+            <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+          </div>
         </div>
       </div>
     </div>
@@ -40,13 +53,12 @@
 </template>
 <script>
   import BScroll from 'better-scroll';
+  import { formatDate } from 'common/js/date';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
   import split from 'components/split/split';
   import ratingselect from 'components/ratingselect/ratingselect';
 
   const ALL = 2;
-  const POSITIVE = 0;
-  const NEGATIVE = 1;
 
   export default {
     name: 'food',
@@ -104,6 +116,22 @@
         this.$nextTick(() => {
           this.scroll.refresh();
         });
+      },
+      needShow(type, text) {
+        if(this.onlyContent && !text) {
+          return false;
+        }
+        if(this.selectType === ALL) {
+          return true;
+        } else {
+          return this.selectType === type;
+        }
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
       }
     },
     components: {
@@ -114,6 +142,8 @@
   }
 </script>
 <style lang="stylus" scoped>
+  @import '../../common/stylus/mixin.styl';
+  
   .move-enter-active, .move-leave
     transition: all 0.2s linear
     transform: translate3d(0, 0, 0)
@@ -219,4 +249,41 @@
           line-height: 14px
           color: rgb(7, 17, 27)
           margin-left: 18px
+        .rating-wrapper
+          padding: 0 18px
+          .rating-item
+            position: relative
+            padding: 16px 0
+            border-1px(rgba(7, 17, 27, 0.1))
+            .user
+              position: absolute
+              right: 0
+              top: 16px
+              font-size: 0
+              line-height: 12px
+              .avatar
+                border-radius: 50%
+              .name
+                display: inline-block
+                vertical-align: top
+                margin-right: 6px
+                font-size: 10px
+                color: rgb(147, 153, 159)
+            .time
+              margin-bottom: 6px
+              font-size: 10px
+              color: rgb(147, 153, 159)
+              line-height: 12px
+            .text
+              font-size: 12px
+              color: rgb(7, 17, 27)
+              line-height: 16px
+              .icon-thumb_up, .icon-thumb_down
+                line-height: 24px
+                margin-right: 4px
+              .icon-thumb_up
+                color: rgb(0, 160, 220)
+              .icon-thumb_down
+                color: rgb(147, 153, 159)
+                
 </style>
